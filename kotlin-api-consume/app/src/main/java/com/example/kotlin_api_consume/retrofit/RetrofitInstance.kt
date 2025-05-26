f578d291b4d6f87df07ad9d8bb2a2ede023fd1ca
TrainerOscar <pgcalero@uamv.edu.ni>
2025-05-26 17:56:57 -0600

    Archivo de retrofitInstance agregado

---
+++ kotlin-api-consume/app/src/main/java/com/example/kotlin_api_consume/retrofit/RetrofitInstance.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin_api_consume.retrofit
+
+import com.example.kotlin_api_consume.services.ApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8000/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
